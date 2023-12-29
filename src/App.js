f797b4f5d69568eff4e5eae404e811149317ead1
cballrun <carter.k.ball@gmail.com>
2023-12-29 14:35:40 -0500

    Routing update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <div className="App">
         <Router>
           <Routes>
-            <Route path="/home" element={<Home/>}/>
+            <Route path="/" element={<Home/>}/>
             <Route path="/apps" element={<Apps/>}/>
             <Route path="/shop" element={<Shop/>}/>
             <Route path="/login" element={<Login/>}/>
