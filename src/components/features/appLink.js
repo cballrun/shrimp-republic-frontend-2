5b0202fc8f17433481984cc9b53b1cb97898a510
Chase Gaddis <cgaddis36@gmail.com>
2024-01-03 12:06:00 -0500

    create conditional for showing project details

--- src/components/features/appLink.js
+++ src/components/features/appLink.js
@@ -1,18 +1,59 @@
 import styles from '../styles/appLink.module.css'
 import arrow from '../images/triangle-cyan.png'
 
-function AppLink({name, active, seActive, key}) {
+function AppLink({
+    index,
+    active, 
+    setActive, 
+    classification,
+    description,
+    endDate,
+    features,
+    name, 
+    stage,
+    startDate,
+    projectUrl
+  }) {
+    
+    const isActive = () => {
+      console.log("Active var", active)
+      console.log("index var", index)
+
+      if(active === index + 1) {
+        return true
+      } else {
+        return false
+      }
+    }
+
+    const activate = () => {
+      if(isActive()) {
+        setActive(0)
+      } else {
+        setActive(index + 1)
+      }
+    }
 
     return (
         <div className={styles.link}>
-            <div className={styles.name}>
-                {name}
+            <div className={styles.titleRow}>
+              <div className={styles.name}>
+                  {name}
+              </div>
+                <img 
+                    className={isActive() ? styles.activeArrow : styles.inactiveArrow}
+                    src={arrow} 
+                    onClick={() => activate()}
+                    />
             </div>
-            <img 
-                className={styles.arrow}
-                src={arrow} 
-                onClick={() => seActive(key)}
-                />
+              {
+                isActive() ? 
+                <div className={styles.description}>
+                  {description}
+                </div>
+                :
+                null
+              }
         </div>
 
     )
