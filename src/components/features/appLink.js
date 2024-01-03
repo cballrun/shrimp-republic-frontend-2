a9f65c8c0f021c7cafdd45d5073c7cc365a86970
Chase Gaddis <cgaddis36@gmail.com>
2024-01-03 14:21:04 -0500

    create conditionals for hidden contents

--- src/components/features/appLink.js
+++ src/components/features/appLink.js
@@ -1,5 +1,9 @@
 import styles from '../styles/appLink.module.css'
 import arrow from '../images/triangle-cyan.png'
+import chessMeitoLogo from '../images/chessMeitoLogo.png'
+import pTradeLogo from '../images/pTradeLogo.png'
+import gameDriveLogo from '../images/gameDriveLogo.png'
+import shoreCastsLogo from '../images/shoreCastsLogo.png'
 
 function AppLink({
     index,
@@ -14,11 +18,14 @@ function AppLink({
     startDate,
     projectUrl
   }) {
-    
-    const isActive = () => {
-      console.log("Active var", active)
-      console.log("index var", index)
+    const imageHash = {
+      "ChessMeito": chessMeitoLogo,
+      "Shorecasts": shoreCastsLogo,
+      "P-Trade": pTradeLogo,
+      "Game Drive": gameDriveLogo
+    } 
 
+    const isActive = () => {
       if(active === index + 1) {
         return true
       } else {
@@ -35,26 +42,47 @@ function AppLink({
     }
 
     return (
-        <div className={styles.link}>
-            <div className={styles.titleRow}>
-              <div className={styles.name}>
-                  {name}
-              </div>
-                <img 
-                    className={isActive() ? styles.activeArrow : styles.inactiveArrow}
-                    src={arrow} 
-                    onClick={() => activate()}
-                    />
-            </div>
-              {
-                isActive() ? 
-                <div className={styles.description}>
-                  {description}
+          <div className={styles.main}>
+            {
+              isActive() || active === 0 ?
+            <div className={styles.link}>
+              <div className={isActive() ? styles.activeTitleRow : styles.inactiveTitleRow}>
+                <img src={imageHash[name]} className={styles.logo}/>
+                <div className={styles.name}>
+                    {name}
                 </div>
-                :
-                null
-              }
-        </div>
+                  <img 
+                      className={isActive() ? styles.activeArrow : styles.inactiveArrow}
+                      src={arrow} 
+                      onClick={() => activate()}
+                      />
+              </div>
+                {
+                  isActive() ? 
+                  <div className={styles.details}>
+                    <div className={styles.description}>
+                      {description}
+                    </div>
+                    <div className={styles.featuresRow}>
+                      <div className={styles.featuresHeader}>
+                        Features
+                      </div>
+                      <div className={styles.features}>
+                      feee
+                      </div>
+                    </div>
+                  </div>
+                  :
+                  null
+                }
+            </div>
+              :
+              null
+            }
+
+          </div>
+
+       
 
     )
 }
