7ce57eb07adeab79fd1a11d5c83bb4a3a292f4f8
cballrun <carter.k.ball@gmail.com>
2024-01-04 16:25:10 -0500

    Extract splash content into splashLower and splashUpper components

---
+++ src/components/features/splashLower.js
@@ -0,0 +1,35 @@
+import styles from '../styles/splash.module.css'
+
+function SplashLower() {
+    return (
+        <div className={styles.splashTextLower}>
+        <div className={styles.appText}>
+            WHETHER YOU'RE ENJOYING
+        </div>
+        <div className={styles.appText}>
+            CHESSMEITO'S PUZZLES OR
+        </div>
+        <div className={styles.appText}>
+            PLANNING A FISHING TRIP WITH
+        </div>
+        <div className={styles.appText}>
+            SHORECASTS, OUR APPLICATIONS
+        </div>
+        <div className={styles.appText}>
+            ARE PURPOSE-BUILT FOR REAL-
+        </div>
+        <div className={styles.appText}>
+            WORLD USE.
+        </div>
+        <div className={styles.appText}>
+            <br></br>
+            WHERE TECHNICAL EXPERTISE
+        </div>
+        <div className={styles.appText}>
+            MEETS EVERYDAY UTILITY.
+        </div>
+    </div>
+    )
+}
+
+export default SplashLower
--- src/components/screens/splash.js
+++ src/components/screens/splash.js
@@ -1,6 +1,8 @@
 import styles from '../styles/splash.module.css'
 import Header from '../features/header.js'
 import Footer from '../features/footer.js'
+import SplashUpper from '../features/splashUpper.js'
+import SplashLower from '../features/splashLower.js'
 import grouperLogo from '../images/grouperLogo.png'
 import { useState } from 'react'
 
@@ -10,39 +12,8 @@ function Splash({projects}) {
     return(
         <div className={styles.main}>
             <Header/>
-            <div className={styles.splashTextUpper}>
-                <div className={styles.exploreBox}>
-                    <div className={styles.exploreText}>EXPLORE SHRIMP </div>
-                    <div className={styles.exploreText}>REPUBLIC</div>
-                </div>
-            </div>
-            <div className={styles.splashTextLower}>
-                <div className={styles.appText}>
-                    WHETHER YOU'RE ENJOYING
-                </div>
-                <div className={styles.appText}>
-                    CHESSMEITO'S PUZZLES OR
-                </div>
-                <div className={styles.appText}>
-                    PLANNING A FISHING TRIP WITH
-                </div>
-                <div className={styles.appText}>
-                    SHORECASTS, OUR APPLICATIONS
-                </div>
-                <div className={styles.appText}>
-                    ARE PURPOSE-BUILT FOR REAL-
-                </div>
-                <div className={styles.appText}>
-                    WORLD USE.
-                </div>
-                <div className={styles.appText}>
-                    <br></br>
-                    WHERE TECHNICAL EXPERTISE
-                </div>
-                <div className={styles.appText}>
-                    MEETS EVERYDAY UTILITY.
-                </div>
-            </div>
+            <SplashUpper/>
+            <SplashLower/>
             <img src={grouperLogo} alt="Grouper Logo" className={styles.grouperLogo} />
             <Footer/>
         </div>
