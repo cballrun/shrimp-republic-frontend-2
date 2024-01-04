803a7f76d2207146e6476fbfa23a53aea548c7ce
Chase Gaddis <cgaddis36@gmail.com>
2024-01-04 14:25:40 -0500

    create comoponent for member

---
+++ src/components/features/member.js
@@ -0,0 +1,30 @@
+import styles from '../styles/member.module.css'
+import chaseHeadshot from '../images/chaseHeadshot.png'
+import carterHeadshot from '../images/awsLogo.png'
+
+function Member({member}) {
+  const headshotDisctionary = {
+    "Chase Gaddis": chaseHeadshot,
+    "Carter Ball": carterHeadshot
+  }
+
+  return (  
+    <div className={styles.main}>
+      <div className={styles.descriptionLocationColumn}>
+        <div className={styles.description}>
+          {member.description}
+        </div>
+        <div className={styles.location}>
+          {`Geographical Location: ${member.location}`} 
+        </div>
+
+      </div>
+      <div className={styles.imageColumn}>
+        <img src={headshotDisctionary[member.name]} className={styles.headshot}/>
+        {member.name}
+      </div>
+    </div>
+  )
+}
+
+export default Member
