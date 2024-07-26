a29721a634fa0eda227f86dc076db74544bf16f8
Chase Gaddis <cgaddis36@gmail.com>
2024-07-26 16:33:09 -0400

    input contact info

--- src/components/features/footer.js
+++ src/components/features/footer.js
@@ -4,16 +4,32 @@ import IconBar from './iconBar'
 function Footer() {
     return (
         <div className={styles.footer}>
-            <text className={styles.footerText}>
-                CUSTOM SOLUTIONS 
-            </text>
-            <text className={styles.footerTextMid}>
-               RAPID DEPLOYMENT
-            </text>
-            <text className={styles.footerText}>
-               WEB AND MOBILE
-            </text>
+            <div className={styles.column}>
+              <text className={styles.footerText}>
+                  CUSTOM SOLUTIONS 
+              </text>
+              <text className={styles.footerTextMid}>
+                RAPID DEPLOYMENT
+              </text>
+              <text className={styles.footerText}>
+                WEB AND MOBILE
+              </text>
+            </div>
             <IconBar/>
+            <div className={styles.infoColumn}>
+              <text className={styles.infoHeader}>
+                Contact Information
+              </text>
+              <text className={styles.infoText}>
+                (850) 582-2223
+              </text>
+              <text className={styles.infoText}>
+                3165 Grand Ave APT 407
+              </text>
+              <text className={styles.infoText}>
+                Pinellas Park, FL 33782
+              </text>
+            </div>
         </div>
     )
 }
