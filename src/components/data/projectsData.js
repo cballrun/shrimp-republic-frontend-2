2223e941323d56ef6f0b558fd2b615b3bdc2d433
cballrun <carter.k.ball@gmail.com>
2024-08-19 14:00:24 -0400

    projectsData content updates

--- src/components/data/projectsData.js
+++ src/components/data/projectsData.js
@@ -1,9 +1,21 @@
 const projectsData = [
     {
+        name: "ShoreCasts",
+        url: "https://www.shorecasts.com",
+        description: "Dive into ShoreCasts, a practical tool offering anglers a seamless blend of tides and weather as well as the locations of thousands of fishing stores, boat ramps, and marinas across the United States. Whether planning your next fishing trip or deciding when and where to put your boat in, ShoreCasts has you covered. Our data is geared toward people on the water and includes first/last light, lunar phase, and granular tidal data including the location of over 1300 NOAA tidal prediction stations.",
+        features: ["Tidal Predictions", "Dynamic Map", "Fishing Shops", "Marinas", "Boat Ramps", "Weather Forecasts"],
+        techs: ["Ruby on Rails", "GraphQL", "PostgreSQL", "JavaScript", "AWS", "Figma"],
+        classification: "Personal",
+        stage: "Production",
+        start: "June 2022",
+        end: "Ongoing",
+    },
+    
+    {
         name: "ChessMeito", 
         url: "https://www.chessmeito.com",
         description: "ChessMeito, where chess meets history. Enjoy two curated puzzles a day with the free version or subscribe and dive into over 6,000 puzzles drawn from actual grandmaster matches, putting you in the shoes of chess legends. ChessMeito's React Native mobile application is available on both the App Store and Play Store and utilizes a Python backend hosted on AWS.",
-        features: ["Chess Puzzles, Updated Daily", "Play & App Store Payments", "Glassfy", "Monthly Subscriptions", "Free & Premium Versions"],
+        features: ["Daily Chess Puzzles", "App & Play Store Payments", "Glassfy", "Monthly Subscriptions", "Free & Premium Versions"],
         techs: ["Test Flight", "Android", "JavaScript", "AWS", "React", "Figma"],
         classification: "Contract Work",
         stage: "Production",
@@ -12,40 +24,28 @@ const projectsData = [
     },
 
     {
-        name: "ShoreCasts",
-        url: "https://www.shorecasts.com",
-        description: "Dive into Shorecasts, a practical tool by Chase Gaddis offering anglers a seamless blend of weather insights, tide charts, and fishing stores near them. Designed as a progressive web and mobile app with a focus on Rails, React, and AWS, Shorecasts centralizes essential tools for planning your days on the water. Discover fishing shop locations, marina services, fly shop details, and insightful reviews on local fishing guides. Shorecasts simplifies the planning process, ensuring your angling adventures are well-informed and tailored to the tides and weather.",
-        features: ["Fishing Shop Locator", "Fly Shop Services & Locator", "Marina Services", "Tidal Predictions", "Weather Forecasts"],
-        techs: ["Ruby on Rails", "GraphQL", "PostgreSQL", "JavaScript", "AWS", "Figma"],
-        classification: "Personal",
-        stage: "Rebuild",
-        start: "June 2022",
-        end: "Ongoing",
+        name: "Game Drive",
+        url: "http://gamedev.ucode.run/auth/sign-in", 
+        description: "Game Drive connects gamers on a personalized level and elevates their online presence. Designed for serious gamers and e-sports enthusiasts, Game Drive offers detailed match-by-match and lifetime stat tracking for titles such as Counter-Strike 2, League of Legends, and Dota 2. By utilizing the Steam and Riot API’s as well as account integration, the platform incorporates users’ libraries and playtime metrics to craft a tailored new feed. All of this information is displayed on a user’s profile, which includes posts and the ability to follow other users. Players can also use Game Drive to create, join, and manage e-sports teams and rosters.",
+        features: ["Stat Tracking", "E-Sports Social Media", "Teams Portal", "Steam/Riot Integration"],
+        techs: ["Go", "PostgreSQL", "JavaScript", "AWS", "React", "Figma"],
+        classification: "Contract Work",
+        stage: "User Testing",
+        start: "July 2023",
+        end: "November 2023",
     },
     
     {
         name: "P-Trade", 
         url: "https://github.com/cballrun/poketrader_2",
         description: "P-Trade is a web application designed to be the central hub for online Pokemon card trading in the United States. Users can effortlessly search for and add cards to their online collection, complete with images, and engage in personalized buy, sell, and trade transactions within a community-oriented format.  P-Trade provides a comprehensive and user-centric platform for Pokemon card enthusiasts to show off their cards and trade like they used to back on the schoolyard.",
-        features: ["Online Pokemon Card Hub", "User Following", "Messaging System", "Notifications", "Collection Showcasing & Tracking", "Card Valuation"],
-        techs: ["Ruby on Rails", "GraphQL", "PostgreSQL", "JavaScript", "AWS", "React", "Figma"],
+        features: ["Online Pokemon Card Hub", "Collection Showcasing & Tracking", "Card Valuation Algorithm"],
+        techs: ["Ruby on Rails", "GraphQL", "PostgreSQL", "JavaScript", "React", "Figma"],
         classification: "Personal",
         stage: "Wireframing",
         start: "November 2023",
         end: "Ongoing",
     },
-    
-    {
-        name: "Game Drive",
-        url: "http://gamedev.ucode.run/auth/sign-in", 
-        description: "Game Drive connects gamers on a personalized level, elevating their online presence and providing a gateway to potential career opportunities. Designed for serious gamers and e-sports enthusiasts, Game Drive offers detailed match-by-match and lifetime stat tracking for titles such as League of Legends, Counter-Strike 2, and Dota 2. Utilizing Steam, Riot, and Google OAuth integrations as well as the Steam and Riot APIs, the platform also incorporates users' libraries and playtime metrics to craft a tailored news feed.  All of this information is displayed on a user's profile, which also includes posts and the capability to follow other users.",
-        features: ["PC Gaming Statistics Tracking", "E-Sports Gaming Community", "User Posts and Comments", "Followers/Following", "E-Sports Teams Portal"],
-        techs: ["Go", "PostgreSQL", "JavaScript", "AWS", "React", "Figma"],
-        classification: "Contract Work",
-        stage: "MVP User Testing",
-        start: "July 2023",
-        end: "November 2023",
-    }
 ]
 
 export default projectsData
