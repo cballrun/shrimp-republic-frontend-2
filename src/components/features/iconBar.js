6767c3598af0e0b98411e3c9c6a07809a7dfa079
cballrun <carter.k.ball@gmail.com>
2024-10-23 17:56:03 -0400

    Footer icon updates for web and mobile

--- src/components/features/iconBar.js
+++ src/components/features/iconBar.js
@@ -1,19 +1,19 @@
 import styles from '../styles/footer.module.css'
 import chessMeitoLogo from '../images/chessMeitoLogo.png'
-import flyFishCircleLogo from '../images/flyFishCircleLogo.png'
+import gameDriveLogo from '../images/gameDriveLogo.png'
 import shoreCastsLogo from '../images/shoreCastsLogo.png'
 
 function IconBar() {
   return (
     <div className={styles.iconBar}>
-        <a href="https://chessmeito.com">
-          <img src={chessMeitoLogo} alt="Chess Meito Icon" className={styles.chessMeitoIcon} />
-        </a>
         <a href="https://www.shorecasts.com">
           <img src={shoreCastsLogo} alt="ShoreCasts Icon" className={styles.shoreCastsIcon} />
         </a>
-        <a href="https://chasegaddis.com">
-          <img src={flyFishCircleLogo} alt="Shrimp Republic Icon" className={styles.shrimpRepublicIcon} />
+        <a href="https://play.google.com/store/apps/details?id=com.chessmeitomobile&hl=en_US">
+          <img src={chessMeitoLogo} alt="Chess Meito Icon" className={styles.chessMeitoIcon} />
+        </a>
+        <a href="https://gamedrive.io">
+          <img src={gameDriveLogo} alt="Game Drive Icon" className={styles.gameDriveIcon} />
         </a>
     </div>
   )
